e841694fc5c3b6c1d0f5df29a49b909266479ecd
uday-shetty <uday.shetty@docker.com>
2017-09-21 12:22:42 -0700

    changed UCP port to 443

--- install-dockeronenode.sh
+++ install-dockeronenode.sh
@@ -123,7 +123,7 @@ docker run --rm --name ucp \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /tmp/docker_subscription.lic:/config/docker_subscription.lic \
   docker/ucp:$UCP_VERSION \
-  install --controller-port 12390 --san $UCP_PUBLIC_FQDN --external-service-lb $APP_ELB_HOSTNAME --admin-password $UCP_ADMIN_PASSWORD
+  install --controller-port 443 --san $UCP_PUBLIC_FQDN --external-service-lb $APP_ELB_HOSTNAME --admin-password $UCP_ADMIN_PASSWORD
 
 # Check if UCP is installed, if not sleep for 15
 if [[ $(curl --insecure --silent --output /dev/null --write-out '%{http_code}' https://"$UCP_PUBLIC_FQDN"/_ping) -ne 200 ]];
